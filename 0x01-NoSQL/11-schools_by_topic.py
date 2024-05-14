28ef9aa33893c53b9f5fa70c7287969b06ef7194
Dan9el8 <mutetemifloice@gmail.com>
2024-05-14 16:50:08 +0000

    nosql

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,13 @@
+eturns list of schools having a specific topic
+Prototype: def schools_by_topic(mongo_collection, topic):
+mongo_collection will be a pymongo collection object
+topic (String) will be topic searched
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    Prototype: def schools_by_topic(mongo_collection, topic):
+    Return list of schools having a specific topic
+    """
+    return mongo_collection.find({"topics": topic})
---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,14 @@
+st all documents in a collection
+Prototype: def list_all(mongo_collection):
+Return an empty list if no document in the collection
+mongo_collection will be the pymongo collection object
+"""
+
+
+def list_all(mongo_collection):
+    """
+    Prototype: def list_all(mongo_collection)
+    Return an empty list if no document in the collection
+    """
+    documents = mongo_collection.find()
+    return documents
